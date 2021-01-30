afdee73ebbfcf16790c1c3a6cb1679dc1a0ed4c2
Antoine Beyeler <abeyeler@ab-ware.com>
2021-01-30 14:26:28 +0100

    Fixed scale

--- {{cookiecutter.sketch_name}}/sketch_{{cookiecutter.sketch_slug}}.py
+++ {{cookiecutter.sketch_name}}/sketch_{{cookiecutter.sketch_slug}}.py
@@ -3,7 +3,7 @@ import vsketch
 
 def setup(vsk: vsketch.Vsketch) -> None:
     vsk.size("{{cookiecutter.page_size}}", landscape={{cookiecutter.landscape}})
-    vsk.scale("{{cookiecutter.preferred_unit")
+    vsk.scale("{{cookiecutter.preferred_unit}}")
 
 
 def draw(vsk: vsketch.Vsketch) -> None:
