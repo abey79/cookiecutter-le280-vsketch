1e25b2956ec3c880d55472d85df7a5fa8d25433d
Antoine Beyeler <abeyeler@ab-ware.com>
2021-03-26 12:26:59 +0100

    Updated for latest version of vsketch

--- {{cookiecutter.sketch_name}}/sketch_{{cookiecutter.sketch_slug}}.py
+++ {{cookiecutter.sketch_name}}/sketch_{{cookiecutter.sketch_slug}}.py
@@ -1,23 +1,20 @@
 import vsketch
 
 
-class {{cookiecutter.class_name}}(vsketch.Vsketch):
+class {{cookiecutter.class_name}}(vsketch.SketchClass):
     # Sketch parameters:
     # radius = vsketch.Param(2.0)
 
-    def draw(self) -> None:
-        self.size("{{cookiecutter.page_size}}", landscape={{cookiecutter.landscape}})
-        self.scale("{{cookiecutter.preferred_unit}}")
+    def draw(self, vsk: vsketch.Vsketch) -> None:
+        vsk.size("{{cookiecutter.page_size}}", landscape={{cookiecutter.landscape}})
+        vsk.scale("{{cookiecutter.preferred_unit}}")
 
         # implement your sketch here
-        # self.circle(0, 0, self.radius(), mode="radius")
+        # vsk.circle(0, 0, self.radius(), mode="radius")
 
-    def finalize(self) -> None:
-        self.vpype("linemerge linesimplify reloop linesort")
+    def finalize(self, vsk: vsketch.Vsketch) -> None:
+        vsk.vpype("linemerge linesimplify reloop linesort")
 
 
 if __name__ == "__main__":
-    vsk = {{cookiecutter.class_name}}()
-    vsk.draw()
-    vsk.finalize()
-    vsk.display()
+    {{cookiecutter.class_name}}.display()
